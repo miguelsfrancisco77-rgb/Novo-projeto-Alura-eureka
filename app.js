9fd016930ea5973963fba1d6624b48ec38c5d75f
Miguel da Silva Francisco <miguelsfrancisco77@gmail.com>
2025-11-15 11:04:44 -0300

    Adiciando codigo para nao repetir numeros sorteados

--- app.js
+++ app.js
@@ -6,7 +6,12 @@ function sortear(){
   let numeros;
   for(let i = 0; i < quantidade; i++){
     numeros = obterNumeroAleatorio(de, ate);
+   let quantidadeDeElementosNaLista = numeros.length;
+    if(sorteados.includes(numeros)){
+      return sortear();
+    } else{
     sorteados.push(numeros);
+    }
   }
   resultado = document.getElementById("resultado");
   resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${sorteados}</label>`;
