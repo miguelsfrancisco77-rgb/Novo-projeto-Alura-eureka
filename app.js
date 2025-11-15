70256130f4ddf7adf66e3306b56384bcf57bc794
Miguel da Silva Francisco <miguelsfrancisco77@gmail.com>
2025-11-15 11:07:26 -0300

    update app.js

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
-function sortear(){
-  let quantidade = parseInt(document.getElementById("quantidade").value);
-  let de = parseInt(document.getElementById("de").value);
-  let ate = parseInt(document.getElementById("ate").value);
-  let sorteados = [];
-  let numeros;
-  for(let i = 0; i < quantidade; i++){
-    numeros = obterNumeroAleatorio(de, ate);
-   let quantidadeDeElementosNaLista = numeros.length;
-    if(sorteados.includes(numeros)){
-      return sortear();
-    } else{
-    sorteados.push(numeros);
+function sortear() {
+    let quantidade = parseInt(document.getElementById("quantidade").value);
+    let de = parseInt(document.getElementById("de").value);
+    let ate = parseInt(document.getElementById("ate").value);
+    let sorteados = [];
+    let numeros;
+    for (let i = 0; i < quantidade; i++) {
+        numeros = obterNumeroAleatorio(de, ate);
+        if (sorteados.includes(numeros)) {
+            return sortear();
+        } else {
+            sorteados.push(numeros);
+        }
     }
-  }
-  resultado = document.getElementById("resultado");
-  resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${sorteados}</label>`;
+    resultado = document.getElementById("resultado");
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${sorteados}</label>`;
 }
-function obterNumeroAleatorio(min, max){
-  return Math.floor(Math.random() * (max - min) + min);
+function obterNumeroAleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
 }
