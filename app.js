cadb06bce047ded805c32e03d40a91eabc5cdc7c
Miguel da Silva Francisco <miguelsfrancisco77@gmail.com>
2025-11-16 02:07:16 -0300

    Projeto finalizadoooo app.js

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ function sortear() {
     resultado = document.getElementById("resultado");
     resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${sorteados}</label>`;
     botaoReiniciar()
-    
 }
 function obterNumeroAleatorio(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
@@ -29,4 +28,17 @@ function botaoReiniciar(){
     botao.classList.add("container__botao-desabilitado");
     botao.classList.remove("container__botao");
   }
+}
+function limparCampo(){
+    quantidade = document.getElementById("quantidade");
+    quantidade.value = '';
+    de = document.getElementById("de");
+    de.value = '';
+    ate =document.getElementById("ate");
+    ate.value = '';
+}
+function reiniciar(){
+  limparCampo()
+  resultado = document.getElementById("resultado");
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados:  nenhum ainda</label>`;
 }
